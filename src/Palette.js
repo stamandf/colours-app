07b742c7a4e14966f6ac483b024276c667c25960
Florence <stamandf@gmail.com>
2019-07-24 17:28:17 -0400

    Initial commit. Palette and seeColors added.

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Palette extends React.Component {
+    render() {
+        return (
+            <div className="Palette">
+            {/* Navbar goes here */}
+                <div className="Palette-colors"> {/* bunch of color boxes */}</div> 
+                {/* footer goes here */}
+            </div>
+        );
+    }
+}
