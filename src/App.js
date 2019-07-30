2df71e489dde5751ce452016d8c0cbc6b3ca7fdb
Florence <stamandf@gmail.com>
2019-07-30 14:28:16 -0400

    Add chroma library and color helper to generate colours.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 export default class App extends React.Component {
   render() {
+    console.log(generatePalette(seedColors[4], 5));
     return (
       <div>
           <Palette {...seedColors[4]}/>
