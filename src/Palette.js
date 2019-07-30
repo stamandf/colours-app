550f59d686414bb2bc43a5c553ef2e7230bd7c62
Florence <stamandf@gmail.com>
2019-07-30 19:09:08 -0400

    Add style to color slide.

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ColorBox from './ColorBox';
+import 'rc-slider/assets/index.css';
 import './Palette.css';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+
 
 export default class Palette extends React.Component {
     constructor(props) {
@@ -21,13 +22,15 @@ export default class Palette extends React.Component {
         );
         return (
             <div className="Palette">
-            <Slider 
-                defaultValue={level} 
-                min={100} 
-                max={900}
-                step={100} 
-                onAfterChange={this.changeLevel}    
-            />
+                <div className="slider">
+                    <Slider 
+                        defaultValue={level} 
+                        min={100} 
+                        max={900}
+                        step={100} 
+                        onAfterChange={this.changeLevel}    
+                    />
+                </div>
             {/* Navbar goes here */}
                 <div className="Palette-colors">{colorBoxes}</div> 
                 
