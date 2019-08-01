cb89528b0605f73dd3826c28be0431becf2ca1c8
Florence <stamandf@gmail.com>
2019-08-01 11:09:45 -0400

    Add Palette footer.

--- src/Palette.js
+++ src/Palette.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ColorBox from './ColorBox';
-// import 'rc-slider/assets/index.css';
 import './Palette.css';
 import Navbar from './Navbar';
-// import Slider from 'rc-slider';
 
 
 export default class Palette extends React.Component {
@@ -20,11 +18,10 @@ export default class Palette extends React.Component {
         this.setState({ format: val });
     }
     render() {
-        const { colors } = this.props.palette;
+        const { colors, paletteName, emoji } = this.props.palette;
         const { level, format } = this.state;
         const colorBoxes = colors[level].map(color => 
-            // <ColorBox background={color.hex} name={color.name} />
-            <ColorBox background={color[format]} name={color.name} />
+            <ColorBox background={color[format]} name={color.name} key={color.id} />
         );
         return (
             <div className="Palette">
@@ -34,7 +31,10 @@ export default class Palette extends React.Component {
                     handleChange={this.changeFormat}
                     />
                 <div className="Palette-colors">{colorBoxes}</div> 
-                
+                <footer className="Palette-footer">
+                {paletteName}
+                 <span className="emoji">{emoji}</span>   
+                </footer>
                 {/* footer goes here */}
             </div>
         );
