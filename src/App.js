7b60baa6902c8ce042aa91b95c2aafc1224f3e78
Florence <stamandf@gmail.com>
2019-08-01 11:55:45 -0400

    Added react-router and created palette route.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 export default class App extends React.Component {
+  findPalette(id) {
+    return seedColors.find((palette)=> (palette.id === id))
+  }
   render() {
-    console.log(generatePalette(seedColors[4], 5));
     return (
-      <div>
-          <Palette palette={generatePalette(seedColors[4])}/>
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+        <Route 
+          exact 
+          path='/palette/:id'
+          render={routeProps => (
+            <Palette 
+                palette= {generatePalette(
+                  this.findPalette(routeProps.match.params.id)
+                )}
+            />
+          )}
+        />
+      </Switch>
+      // <div>
+      //     <Palette palette={generatePalette(seedColors[4])}/>
+      // </div>
     )
   }
 }
