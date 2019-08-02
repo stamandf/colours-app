8c3c0e671595757e5e748ae8e2e29bab95713b7d
Florence <stamandf@gmail.com>
2019-08-02 11:51:58 -0400

    Add Material UI styles, Mini Palette and mini colour boxes.

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,18 +1,54 @@
 import React from 'react';
+import MiniPalette from './MiniPalette';
 import { Link } from 'react-router-dom';
-export default class PaletteList extends React.Component {
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+    root: {
+        backgroundColor: "blue",
+        height: "100vh",
+        display: "flex",
+        alightItems: "flex-start",
+        justifyContent: "center"
+    },
+    container: {
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+        flexWrap: "wrap",
+    },
+    nav: {
+        display: "flex",
+        width: "100",
+        justifyContent: "space-between",
+        color: "white"
+    },
+    palettes: {
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 30%)",
+        gridGap: "5%"
+    }
+}
+class PaletteList extends React.Component {
     render() {
-        const { palettes } = this.props;
+        const { palettes, classes } = this.props;
         return (
-            <div>
-                <h1>Palette List</h1>
-                {palettes.map(palette => (
-                    <p>
-                    <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-                    </p>
-                    
-                ))}
+            <div className={classes.root}>
+                <div className={classes.container}>
+                    <nav className={classes.nav}>
+                        <h1>React Colors</h1>
+                    </nav>
+                    <div className={classes.palettes}>
+                        {palettes.map(palette => (
+                            <MiniPalette {...palette} />
+                        ))}
+                    </div>
+                </div>
             </div>
         )
     }
 }
+export default withStyles(styles)(PaletteList);
