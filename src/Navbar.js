2060850dcd092cc92175be00b0e2dd2b1c0eade5
Florence <stamandf@gmail.com>
2019-08-04 12:21:44 -0400

    Refactor Navbar styles and remove CSS files.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'rc-slider/assets/index.css';
-import './Navbar.css';
 import Slider from 'rc-slider';
 import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
-
 import Select from '@material-ui/core/Select';
+import { withStyles } from '@material-ui/styles';
+import styles from './styles/NavbarStyles';
 
-
-export default class Palette extends React.Component {
+class Navbar extends React.Component {
     constructor(props) {
         super(props);
         this.state = { format: "hex", open: false };
@@ -26,17 +25,17 @@ export default class Palette extends React.Component {
         this.setState({ open: false });
     }
     render() {
-        const { level, changeLevel, showingAllColors } = this.props;
+        const { level, changeLevel, showingAllColors, classes } = this.props;
         const { format } = this.state;
         return (
-            <header className="Navbar">
-                <div className="logo">
+            <header className={classes.Navbar}>
+                <div className={classes.logo}>
                     <Link to="/">reactcolorpicker</Link>
                 </div>
                 {showingAllColors && (
-                    <div className='slider-container'>
+                    <div>
                         <span>Level:{level}</span>
-                        <div className="slider">
+                        <div className={classes.slider}>
                             <Slider 
                                 defaultValue={level} 
                                 min={100} 
@@ -48,7 +47,7 @@ export default class Palette extends React.Component {
                     </div>
                 )}
                 
-                <div className="select-container">
+                <div className={classes.selectContainer}>
                     <Select value={format} onChange={this.handleFormatChange}>
                         <MenuItem value={"hex"}>HEX - #fffffff</MenuItem>
                         <MenuItem value={"rgb"}>RGB - rgb(255, 255, 255)</MenuItem>
@@ -79,3 +78,4 @@ export default class Palette extends React.Component {
         );
     }
 }
+export default withStyles(styles)(Navbar);
