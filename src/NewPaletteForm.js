ebe963f066a38c274905f32e546fb8bd88c5e3b4
Florence <stamandf@gmail.com>
2019-08-05 18:22:32 -0400

    Refactor New Palette Nav to own component.

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PaletteFormNav from './PaletteFormNav';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
@@ -87,7 +89,6 @@ class NewPaletteForm extends React.Component {
             currentColor: "teal",
             colors: this.props.palettes[0].colors,
             newColorName: "",
-            newPaletteName: ""
           };
     }
     
@@ -130,8 +131,7 @@ class NewPaletteForm extends React.Component {
             };
           this.setState({ colors: [...this.state.colors, newColor], newColorName: "" });
       }
-      handleSubmit = () => {
-        let newPaletteName = this.state.newPaletteName;
+      handleSubmit = (newPaletteName) => {
         const newPalette = {
             paletteName: newPaletteName, 
             id: newPaletteName.toLowerCase().replace(/ /g,"-"),
@@ -164,47 +164,18 @@ class NewPaletteForm extends React.Component {
       }
 
       render() {
-        const { classes, maxColors } = this.props;
+        const { classes, maxColors, palettes } = this.props;
         const { open, colors } = this.state;
         const paletteIsFull = colors.length >= maxColors;
     
         return (
           <div className={classes.root}>
-            <CssBaseline />
-            <AppBar
-              position="fixed"
-              color="default"
-              className={classNames(classes.appBar, {
-                [classes.appBarShift]: open,
-              })}
-            >
-              <Toolbar disableGutters={!open}>
-                <IconButton
-                  color="inherit"
-                  aria-label="Open drawer"
-                  onClick={this.handleDrawerOpen}
-                  className={classNames(classes.menuButton, open && classes.hide)}
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" color="inherit" noWrap>
-                  Persistent drawer
-                </Typography>
-                <ValidatorForm onSubmit={this.handleSubmit} ref='form'>
-                    <TextValidator 
-                        label="Palette Name"
-                        name="newPaletteName" 
-                        value={this.state.newPaletteName} 
-                        onChange={this.handleChange} 
-                        validators={["required", "isPaletteNameUnique"]}
-                        errorMessages={["Enter Palette Name", "Palette Name already in use!"]}
-                        />
-                    <Button variant="contained" color="primary" type="submit">
-                        Save Palette
-                    </Button>
-                </ValidatorForm>
-              </Toolbar>
-            </AppBar>
+            <PaletteFormNav 
+                open={open} 
+                classes={classes} 
+                palettes={palettes} 
+                handleSubmit={this.handleSubmit}
+                handleDrawerOpen={this.handleDrawerOpen} />
             <Drawer
               className={classes.drawer}
               variant="persistent"
