14912b01206e8352731b400c5e6fd28cb3258275
Florence <stamandf@gmail.com>
2019-08-08 14:53:55 -0400

    Extra styling and change Form Nav icon.

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -6,7 +6,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import PaletteIcon from '@material-ui/icons/Palette';
+import BrushIcon from '@material-ui/icons/Brush';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import PaletteMetaForm from './PaletteMetaForm';
@@ -50,9 +51,10 @@ class PaletteFormNav extends React.Component {
                   color="inherit"
                   aria-label="Open drawer"
                   onClick={this.props.handleDrawerOpen}
-                  className={classNames(classes.menuButton, open && classes.hide)}
+                  className={classNames(classes.menuButton, { [classes.hide]: open })}
                 >
-                  <MenuIcon />
+                  <PaletteIcon />
+                  <BrushIcon />
                 </IconButton>
                 <Typography variant="h6" color="inherit" noWrap>
                   Create a Palette
