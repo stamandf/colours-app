781f9f5984a5bf776b0449a7ff06da30ab03f38f
Florence <stamandf@gmail.com>
2019-12-05 17:22:12 -0500

    Updates

---
+++ precache-manifest.b53d655078ca0846d696a0b6f29eb692.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45865c9cef69c0a288afdd0ce894dd12",
+    "url": "/colours-app/index.html"
+  },
+  {
+    "revision": "ca3e9acb8fc67df86426",
+    "url": "/colours-app/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "c16fb61f3ec26cbb3c09",
+    "url": "/colours-app/static/css/main.1539f489.chunk.css"
+  },
+  {
+    "revision": "ca3e9acb8fc67df86426",
+    "url": "/colours-app/static/js/2.bba648d9.chunk.js"
+  },
+  {
+    "revision": "c16fb61f3ec26cbb3c09",
+    "url": "/colours-app/static/js/main.6fd18770.chunk.js"
+  },
+  {
+    "revision": "5a35371b20bfb3513e99",
+    "url": "/colours-app/static/js/runtime~main.4f8f5e8f.js"
+  },
+  {
+    "revision": "50688f96ee7a67e3ca5d8a7203e46a84",
+    "url": "/colours-app/static/media/bg.50688f96.svg"
+  }
+]);
